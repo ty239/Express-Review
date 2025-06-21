46b00f9717aa8dae86e307a8752c76b83df88395
Finn Terdal <finnterdal@Finns-MBP.nyc.rr.com>
2025-06-21 01:29:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ⚛️ React Mini!
+
+This is meant to get you started learning React! It uses Webpack and Babel to complile your React code, along with your CSS. You can also use `webpack-dev-server` to preview your React application locally, and the application will rebuild quickly on every file save. By default, the server runs on [http://localhost:3000](http://localhost:3000).
+
+## 🚀 Getting Started
+
+1. Clone or fork this repository on GitHub.
+2. Install the NPM packages: `npm install`
+3. Preview your React app: `npm run start-dev`
+
+If you're curious to see what Webpack is actually sending to the browser, try building the app with `npm start`. Then, look at `public/main.js`. What you find may shock and horrify you. 😱
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
