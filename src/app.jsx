eaa9fbef368548f6f058d3f6f772ca7078661a26
Finn Terdal <finnterdal@Finns-MBP.nyc.rr.com>
2025-06-22 15:34:04 -0400

    adjust app.jsx for better starting point

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
-const App = () => {};
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="title">Hello World</h1>
+    </div>
+  );
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
