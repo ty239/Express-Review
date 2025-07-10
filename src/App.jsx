b9c62798a3ae225e221ee131c3caacd7f426b906
Muhammad <khawajat016@gmail.com>
2025-07-09 23:22:06 -0400

    just stareted with serachbar compent

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./style.css";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
-
+import user from "./components/BookList";
 const App = () => {
   return (
     <div className="app">
       <h1 className="title">React Forms! 📝</h1>
       <AddBook />
       <BookList />
+      < user />
     </div>
   );
 };
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = createRoot(document.getElementById("root")) ;
+root.render(<App />) ;
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,37 +1,40 @@
 import react, { useEffect, useState } from "react";
-import React,{react,useffect} from "react";
-import serachBar from "./serachBar";
+import serachBar from "./searchBar";
 
+function main() {
+  const [userName, setUsername] = useState(""); //hold the username
+  const [userData, setUserData] = useState(null); //hold the user data
+}
 
- const [userName,setUsername] = useState("")//hold the username
- const [userData,setUserData] = useState(null); //hold the user data
+[userData]; //tell my helper what to do //what to look out for
 
+const gitHubUrl = `https://api.github.com/users/${userName}`;
 
- useEffect(() => { //my helper looking for changes
-  getuserData(); //tell my helper what to do
-  [userData];//what to look out for
- })
-  const gitHubUrl = `https://api.github.com/users/${username}`;    
- 
 const checkForValidUrl = async () => {
-    try{
-const response = await fetch(gitHubUrl)// fetch is used to grab the api
-const rewritten = await response.json();// is use to translate the data
-if(!response && rewritten !== "this not real bro"){ //checking for the
-    setUserData(rewritten);
-    console.log(data);
-}
-else {
-        setUserData({});
+  try {
+    const response = await fetch(gitHubUrl); // fetch is used to grab the api
+    const rewritten = await response.json(); // is use to translate the data
+    if (!response.ok && rewritten.message !== "this not real bro") {
+      //checking for the
+      setUserData(rewritten);
+      console.log(rewritten);
+    } else {
+      setUserData({});
     }
+  } catch (err) {
+    console.log("error", err);
+  }
+
+  useEffect(() => {
+    if (userName) {
+      checkForValidUrl();
     }
-    catch(err){
-        console.log("error", err);
-    }
-   
-}
+  }, [userName]);
 
-
-
-
-return <serachBar userName ={userName}></serachBar>
+  return (
+    <div>
+      <serachBar userName={userName} setUserName={setUserName} />
+      <userInfoCard userData={userData} />
+    </div>
+  );
+};
